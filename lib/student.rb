3985d96f8089892dbb3d3c85b6aa01c2d0d823af
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 19:55:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,10 @@ class Student
     end
   end
   
+  def self.first_X_students_in_grade_10
+    self.first_X_students_in_grade_10.first
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
