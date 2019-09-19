35b6e256878f6f3c9d96eaa8e0bd68596e91a342
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 19:25:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    student = Student.new
+    student = Student.new(row)
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
