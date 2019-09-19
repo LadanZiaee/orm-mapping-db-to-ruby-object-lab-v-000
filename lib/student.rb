7fbef4f1cedf175d1b76900d9d219d31f7a29fbc
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 19:39:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     end 
   end
 
-  def self.students_below_12_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students WHERE grade < 12;
      SQL
