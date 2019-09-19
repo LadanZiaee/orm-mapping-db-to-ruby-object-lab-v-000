f2896d939a9871fa7818548873f805ca146b836a
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 20:01:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(3).first
+    self.first_X_students_in_grade_10(2).first
   end
   
   def save
