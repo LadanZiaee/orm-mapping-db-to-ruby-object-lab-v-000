7d8c888d718f7ec9c84fa10deec876efa9fb4917
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 19:39:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT * FROM students WHERE grade < 12;
+      SELECT students.name FROM students WHERE grade < 12;
      SQL
     DB[:conn].execute(sql).map{|row| row}
   end
