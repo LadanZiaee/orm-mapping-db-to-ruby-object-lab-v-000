07a0a9bc35e6cc2261930d9ed8b30988534e2f46
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 19:34:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,13 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9;
+     SQL
+    sql 
+  end
 
   def self.find_by_name(name)
     # find the student in the database given a name
